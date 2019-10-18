5fce709d916520cdeef05074a58134720e088af0
apospraf <apospraf1@hotmail.com>
2019-10-18 12:22:29 +0300

    label blocking and comments

--- experiment_gui/main.php
+++ experiment_gui/main.php
@@ -1,3 +1,4 @@
+<!-- Receive the subject number from the previous page -->
 <?php
     $subname = $_POST["subname"];
 ?>
@@ -7,6 +8,12 @@
 <style type="text/css">
     * {box-sizing:border-box}
     
+    /*Sudo class for disabling highlighting*/
+    ::selection {
+        background-color: transparent;
+        color: #000;
+    }
+
     .slideshow-container {
         max-width: 1000px;
         position: relative;
@@ -17,7 +24,7 @@
     }
 
     /* Hide the images by default */
-    .mySlides {
+    .mySlides::selection{
         display: none;
     }
 
@@ -27,6 +34,16 @@
         -webkit-animation-duration: 1.5s;
         animation-name: fade;
         animation-duration: 1.5s;
+    }
+
+    .block {
+        display: none;
+        background-color: white;
+        position: absolute;
+        border: 2px solid black;
+        padding: 20px 150px;
+        top: 82%;
+        left: 5%
     }
 
     @-webkit-keyframes fade {
@@ -41,8 +58,8 @@
 </style>
 
 <body onmousedown="addAnswer(event)" oncontextmenu="return false;">
-
-    <div class="slideshow-container">
+    
+    <div class="slideshow-container" onmousedown="return false">
         <div class="mySlides fade">
             <img src="./image/-.png" style="width:40%">
         </div>
@@ -82,13 +99,16 @@
         <div class="mySlides fade">
             <img src="./image/8.png" style="width:40%">
         </div>
-
+        <!-- Blocking the label for x secs -->
+        <div class="block" id="blocking"></div>
 
     </div>
 
+    <!-- Slideshow script -->
     <script>
-        var slideIndex = 0;
+        var slideIndex = 0; //current slide index
         var subname = '<?php echo $subname; ?>'
+        var canAnswer = false; //variable that can block mouse answers when needed
         showSlides();
 
         function showSlides() {
@@ -98,29 +118,45 @@
                 slides[i].style.display = "none";
             }
             slideIndex++;
-            if (slideIndex > slides.length) {download(subname + '.txt'); document.location.href = "./end.html"}
+            if (slideIndex > slides.length) {
+                download(subname + '.txt'); // when slideshow ends save file
+                document.location.href = "./end.html" // and change url
+            } 
             slides[slideIndex-1].style.display = "block";
-            if (slideIndex % 2 == 0 ){
-                document.onkeypress = function(e) {
-                    e = e || window.event;
-                }
+            if (slideIndex % 2 == 0 ){ //Slides with odd index are baseline crosses
+                canAnswer = true;
+                document.getElementById("blocking").style.display = "block";
+                setTimeout(function() {
+                    document.getElementById("blocking").style.display = "none"
+                }, 5000);
                 setTimeout(showSlides, 10000);
             } else {
+                canAnswer = false;
+                document.getElementById("blocking").style.display = "none";
                 setTimeout(showSlides, 5000);
             }
         }
 
     </script>
 
+
     <script>
         var answers = new Array(); 
         var times = new Array();
         var startTime = Date.now();
+        // Appends new answer to the answers variable if canAnswer flag is true
+        // Also calculates the milliseconds passed from the beginning to the mouse click
         function addAnswer(event) {
-            answers.push(event.button);
+            if (canAnswer) {
+                answers.push(event.button);
+                document.getElementById("kwlos").innerHTML = answers;
+            } 
+
             times.push(Date.now()-startTime);
+            canAnswer = false;
         }
 
+        // Downloads the final file
         function download(filename) {
             var pom = document.createElement('a');
             pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(answers.toString() + '\n' + times.toString()));
